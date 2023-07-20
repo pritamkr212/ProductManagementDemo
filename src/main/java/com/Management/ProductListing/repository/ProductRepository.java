c57d468b59ebb3c9793bd774cdc89efe797853ab
EmPritam <pritamkr212@gmail.com>
2023-07-20 21:33:59 +0530

    ProductIDchanges

--- src/main/java/com/Management/ProductListing/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Management.ProductListing.repository;
-
-import com.Management.ProductListing.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
